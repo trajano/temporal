9495f29e3f2311872e4722fb7d61daa78ffcb059
Archimedes Trajano <developer@trajano.net>
2019-09-16 18:23:41 -0400

    Example of attribute override for the key

--- src/main/java/net/trajano/temporal/sample/SampleTemporalEntity.java
+++ src/main/java/net/trajano/temporal/sample/SampleTemporalEntity.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import net.trajano.temporal.domain.TemporalEntity;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.time.LocalDate;
 
@@ -12,6 +14,18 @@ import java.time.LocalDate;
 @EqualsAndHashCode(callSuper = true)
 public class SampleTemporalEntity extends TemporalEntity<String, LocalDate> {
 
+    /**
+     * The lookup key.  This specifies an {@link AttributeOverride} so that the length can be specified, note that the
+     * {@code nullable = false} and {@code updatable = false} should also be set as it is not inherited.
+     */
+    @AttributeOverride(
+      name = "key",
+      column = @Column(
+        length = 32,
+        nullable = false,
+        updatable = false
+      )
+    )
     private String key;
 
     private String property;
