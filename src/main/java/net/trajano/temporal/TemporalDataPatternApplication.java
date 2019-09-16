44a40083586afc35a6a7cbeecb6d849db77bd2ba
Archimedes Trajano <arch@trajano.net>
2019-09-16 10:46:48 -0400

    initial commit

---
+++ src/main/java/net/trajano/temporal/TemporalDataPatternApplication.java
@@ -0,0 +1,13 @@
+package net.trajano.temporal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalDataPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalDataPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/trajano/temporal/TemporalDataPatternApplicationTests.java
@@ -0,0 +1,17 @@
+package net.trajano.temporal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class TemporalDataPatternApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
